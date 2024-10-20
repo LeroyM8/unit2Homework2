38e6a997b7a5bffa1f4074f682c7c0c0f89c4f21
leroy <leroy@Leroy>
2024-10-20 12:57:19 +0200

    first commit

---
+++ Unit1/src/unit1/Unit1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit1;
+
+/**
+ *
+ * @author leroy
+ */
+public class Unit1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Project that displays name, student number and number
+        System.out.println("Leroy");
+        System.out.println("ST10097086@rcconnect.edu.za");
+        System.out.println("0812027449");
+    }
+    
+}
